0638eaaec458e692e5aa56457df56c2bc95eede0
Mykhailo Potemko <105733943+potemko@users.noreply.github.com>
2023-06-14 10:05:55 +0300

    4

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/exemple-dashboard">
+    <BrowserRouter basename="/glivera-team">
       <App />
     </BrowserRouter>
   </React.StrictMode>
